1e1241c1e7d6f83b3e03c7de16c7e5ad8f9f2536
Marc Rodriguez-Estivill <mrodriguezestivill@gmail.com>
2020-01-24 16:15:07 +0100

    bump version 1.0.0

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ var (
 )
 
 const mph2kph float64 = 1.60934
-const projectVersion string = "v0.1.0"
+const projectVersion string = "v1.0.0"
 const projectURL string = "https://github.com/glarfs/docker-observerip-proxy-mqtt"
 
 func main() {
